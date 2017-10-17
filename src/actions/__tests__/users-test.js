f4da3ef21c1ce678c6822ab82b7341c43b7bd0b0
Arun Sood <arsood@gmail.com>
2017-10-17 15:42:14 -0700

    GET_ONE_USER_SUCCESS action creator test

--- src/actions/__tests__/users-test.js
+++ src/actions/__tests__/users-test.js
@@ -8,8 +8,6 @@ import axiosConfig from "../../config/axios";
 const mockStore = configureMockStore([thunk]);
 const mockHttp = new MockAdapter(axiosConfig);
 
-const store = mockStore();
-
 describe("USER ACTION CREATORS", () => {
 
   it("Creates USERS_FETCH_SUCCESS", () => {
@@ -17,11 +15,28 @@ describe("USER ACTION CREATORS", () => {
     .onGet("/persons")
     .reply(200, []);
 
+    const store = mockStore();
+
     store
     .dispatch(userActions.getUsers())
     .then(() => {
       expect(store.getActions())
       .toEqual([{type: "USERS_FETCH_SUCCESS", payload: []}]);
+    });
+  });
+
+  it("Creates GET_ONE_USER_SUCCESS", () => {
+    mockHttp
+    .onGet("/persons/1")
+    .reply(200, {});
+
+    const store = mockStore();
+
+    store
+    .dispatch(userActions.getOneUser(1))
+    .then(() => {
+      expect(store.getActions())
+      .toEqual([{type: "GET_ONE_USER_SUCCESS", payload: {}}]);
     });
   });
 
