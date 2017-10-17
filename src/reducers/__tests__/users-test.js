baae5f950eb5225f4d010792d0aac6fe16140752
Arun Sood <arsood@gmail.com>
2017-10-17 14:55:49 -0700

    GET_ONE_USER_SUCCESS reducer test

--- src/reducers/__tests__/users-test.js
+++ src/reducers/__tests__/users-test.js
@@ -24,4 +24,26 @@ describe("USERS REDUCER", () => {
     }));
   });
 
+  it("GET_ONE_USER_SUCCESS", () => {
+    const stateChange = usersReducer(null, {
+      type: "GET_ONE_USER_SUCCESS",
+      payload: {
+        firstname: "Bob",
+        lastname: "Jones",
+        username: "bjones",
+        email: "bjones@gmail.com"
+      }
+    });
+
+    expect(stateChange)
+    .toEqual(expect.objectContaining({
+      oneUser: {
+        firstname: "Bob",
+        lastname: "Jones",
+        username: "bjones",
+        email: "bjones@gmail.com"
+      }
+    }));
+  });
+
 });
